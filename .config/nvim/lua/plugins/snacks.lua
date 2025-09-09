6c967692375460d206d97a740bcc616105369455
Onkar <onkarsathe96k@gmail.com>
2025-09-09 22:10:51 +0530

    chore(nvim): update snacks plugin configuration

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,51 +7,77 @@ return {
         opts = {
             indent = { enabled = true, priority = 1, animate = { enabled = false } },
             notifier = { enabled = true, timeout = 3000 },
-            dashboard = { enabled = true }, -- enabled!
-            explorer = { enabled = false },
-            terminal = { enabled = true },
-            tweaks = { enabled = true }, -- Enable tweaks!
-            background = {
-                enabled = true,
-                image = "~/Pictures/wallpapers/aesthetic2.jpg",
-                opacity = 1,
-                stretch = "fit",
+            -- dashboard = { enabled = true }, -- enabled!
+            dashboard = {
+                preset = {
+                    pick = nil,
+                    ---@type snacks.dashboard.Item[]
+                    keys = {
+                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
+                    header = [[
+                                                                             
+               ████ ██████           █████      ██                     
+              ███████████             █████                             
+              █████████ ███████████████████ ███   ███████████   
+             █████████  ███    █████████████ █████ ██████████████   
+            █████████ ██████████ █████████ █████ █████ ████ █████   
+          ███████████ ███    ███ █████████ █████ █████ ████ █████  
+         ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+      ]],
+                },
+                explorer = { enabled = false },
+                terminal = { enabled = true },
+                tweaks = { enabled = true }, -- Enable tweaks!
+                background = {
+                    enabled = true,
+                    image = "~/Pictures/wallpapers/aesthetic2.jpg",
+                    opacity = 1,
+                    stretch = "fit",
+                },
+                -- ...add more as you like
             },
-            -- ...add more as you like
+            keys = {
+                {
+                    "<leader>lg",
+                    function()
+                        Snacks.lazygit()
+                    end,
+                    desc = "Open LazyGit",
+                },
+                {
+                    "<leader><space>",
+                    function()
+                        Snacks.picker.smart()
+                    end,
+                    desc = "Smart picker",
+                },
+                {
+                    "<leader>0",
+                    function()
+                        require("snacks").dashboard()
+                    end,
+                    desc = "Open Snacks Dashboard",
+                },
+                -- ...other keybindings...
+                {
+                    "<leader>st",
+                    function()
+                        require("snacks").terminal.toggle()
+                    end,
+                    desc = "Toggle Snacks Terminal",
+                },
+                -- add more keybindings as in README examples
+            },
         },
-        keys = {
-            {
-                "<leader>lg",
-                function()
-                    Snacks.lazygit()
-                end,
-                desc = "Open LazyGit",
-            },
-            {
-                "<leader><space>",
-                function()
-                    Snacks.picker.smart()
-                end,
-                desc = "Smart picker",
-            },
-            {
-                "<leader>0",
-                function()
-                    require("snacks").dashboard()
-                end,
-                desc = "Open Snacks Dashboard",
-            },
-            -- ...other keybindings...
-            {
-                "<leader>st",
-                function()
-                    require("snacks").terminal.toggle()
-                end,
-                desc = "Toggle Snacks Terminal",
-            },
-            -- add more keybindings as in README examples
-        },
-    },
 
-    -- more ptgins...
+        -- more plugins...
+    }
 }
